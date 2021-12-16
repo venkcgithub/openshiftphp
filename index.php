e20e2eb2281efca51209cc69dec720841cbfe556
venkcgithub <96234809+venkcgithub@users.noreply.github.com>
2021-12-16 16:09:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>PHP 2021  Test</h1>
-		<p><b>  Welcome venkat to DANSKEIT BANK </b></p>
+		<p><b>  Welcome venkata suresh c to DANSKEIT BANK </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION 2 </h2> 
